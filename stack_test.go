791fce59cb21027b91d53e3d060e60a07d4050a7
Benjamin Boortz <benjamin.boortz@secure.mailbox.org>
2017-01-02 22:51:30 +0100

    stack.go added /BB

---
+++ stack_test.go
@@ -0,0 +1,38 @@
+package utils
+
+import (
+	//"github.com/davecgh/go-spew/spew"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestStack(t *testing.T) {
+	assert := assert.New(t)
+
+	var stack []byte = Stack()
+	assert.NotNil(stack)
+}
+
+func TestPrintStack(t *testing.T) {
+	PrintStack()
+}
+
+func TestTrace(t *testing.T) {
+	Trace()
+}
+
+func TestGetCurrentMethodName(t *testing.T) {
+	assert := assert.New(t)
+
+	var result string = GetCurrentMethodName()
+	assert.NotNil(result)
+	assert.Equal("go-utils.TestGetCurrentMethodName", result)
+}
+
+func TestGetCallingMethodName(t *testing.T) {
+	assert := assert.New(t)
+
+	var result string = GetCallingMethodName()
+	assert.NotNil(result)
+	assert.Equal("testing.tRunner", result)
+}
